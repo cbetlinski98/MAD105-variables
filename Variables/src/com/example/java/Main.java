1ff1e6d9827ff3ef711861c62b4b8dc1163810fd
cbetlinski98 <cbetlinski@students.mchenry.edu>
2018-09-03 18:52:47 -0500

    Variables assignment

---
+++ Variables/src/com/example/java/Main.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 5;
+        short sh = 5;
+        int i = 5;
+        long l = 5L;
+        float f =5f;
+        double d = 5d;
+        boolean bIsThisTrue = true;
+        char c = 'c';
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + sh);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Boolean: " + bIsThisTrue);
+        System.out.println("Char: " + c);
+
+    }
+}
